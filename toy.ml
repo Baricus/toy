d96189a0536e04aba49f092b533f2101f545e75c
Susan Chen <susanchen@Susans-MacBook-Air.local>
2022-11-10 01:24:02 -0600

    added new leapyear function

--- toy.ml
+++ toy.ml
@@ -362,6 +362,11 @@ let fact_acc_str = "
 \\ fact_internal ( dup 1 > ( dup 2 $ * swap 1 - fact_internal ) ( drop 1 * ) if ) define 
 \\ fact ( 1 swap fact_internal ) define 5 fact"
 
+let next_leapyear_str = "
+\\ is_leapyear ( dup 4 % ) define
+\\ next_leapyear ( is_leapyear 0 = ( 4 + ) ( is_leapyear 4 1 $ - + ) if ) define 
+    2021 next_leapyear"
+
 (* Possible Typecheck Cases *)
 
 (*  _ : make sure ...
